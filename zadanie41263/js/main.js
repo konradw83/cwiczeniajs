8dff9b3ce5280c7d7c5a6da2023d4ff7b248c1f4
konradw <wiakkon@gmail.com>
2022-10-24 13:26:25 +0200

    start

--- zadanie41263/js/main.js
+++ zadanie41263/js/main.js
@@ -1,9 +1,12 @@
 let firstNumber = 5;
 let secondNumber = 10;
 
-function suma(firstNumber, secondNumber) {
-    let wynik = firstNumber + secondNumber;
+function suma(num1, num2) {
+    let wynik = num1 + num2;
 
     return wynik;
 }
 
+console.log(suma(firstNumber, secondNumber));
+
+console.log(suma(45, 7));
